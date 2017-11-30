2f71482b242366b52777488b6fe556845d480f64
RafaelNercessian <rafanercessian@gmail.com>
2017-11-30 13:01:47 -0200

    Mudando classe FileSaver

--- src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,29 +1,29 @@
 package br.com.casadocodigo.loja.infra;
 
-import java.io.File;
 import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import br.com.casadocodigo.loja.models.Produto;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Component
 public class FileSaver {
 
 	@Autowired
-	private HttpServletRequest request;
+	private AmazonS3 amazonS3;
+	private static final String BUCKET="casadocodigo-rafaelsilvanercessian";
 
 	public String write(MultipartFile file) {
 		try {
-			File arquivo = new File(request.getServletContext().getRealPath(
-					"/resources/imagens"), file.getOriginalFilename());
-			file.transferTo(arquivo);
-			
-			return file.getOriginalFilename();
+			amazonS3.putObject(new PutObjectRequest(BUCKET, 
+					file.getOriginalFilename(), file.getInputStream(),null)
+					.withCannedAcl(CannedAccessControlList.PublicRead));
+				
+			return "http://s3.amazonaws.com/"+BUCKET+"/"+file.getOriginalFilename();
 			
 		} catch (IllegalStateException | IOException e) {
 			throw new RuntimeException(e);
